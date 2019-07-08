5bc8fb47f0539f4d7cc5707ae52fbeb635c01cc8
Azirel <azirel92@gmail.com>
2019-07-08 17:51:06 +0300

    Init: - Oculus Integration - LWRP standard

---
+++ Assets/Scripts/SRPImageEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BasicPipeInstance : RenderPipeline
+{
+	private Color m_ClearColor = Color.black;
+
+	public BasicPipeInstance(Color clearColor)
+	{
+		m_ClearColor = clearColor;
+	}
+
+	protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+	{
+		// does not so much yet :(
+		//base.Render(context, cameras);
+		Debug.Log("Called by SRP!!!");
+
+		// clear buffers to the configured color
+		var cmd = new CommandBuffer();
+		cmd.ClearRenderTarget(true, true, m_ClearColor);
+		context.ExecuteCommandBuffer(cmd);
+		cmd.Release();
+		context.Submit();
+	}
+}
