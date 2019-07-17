cf3bb8d0bb5372afa24c2ad21e1fc32e8391aae5
Azirel <azirel92@gmail.com>
2019-07-17 18:03:49 +0300

    feat (injection) : injection attempt

--- Assets/Scripts/SRPEventsTest.cs
+++ Assets/Scripts/SRPEventsTest.cs
@@ -15,25 +15,24 @@ public class SRPEventsTest : MonoBehaviour
 	public void Start()
 	{
 		CommandBuffer cmd = new CommandBuffer();
-		//Camera.activeTexture
 		cmd.Blit(Camera.activeTexture, Camera.activeTexture, ImageEffectMaterial);
 		Camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, cmd);
+		ScriptableRenderPass.OnPostProcessEvent += ScriptableRenderPass_OnPostProcessEvent;
+	}
+
+	private void ScriptableRenderPass_OnPostProcessEvent(CommandBuffer cmd, ref CameraData data, RenderTextureDescriptor sourceDescriptor, RenderTargetIdentifier sourceIdentifier, RenderTargetIdentifier destinationIdentifier, bool opaqOnly, bool flip)
+	{
+		
 	}
 
 	protected void OnEnable()
 	{
 		UnityEngine.Experimental.Rendering.RenderPipeline.beginCameraRendering += RenderPipeline_beginCameraRendering;
 		UnityEngine.Experimental.Rendering.RenderPipeline.beginFrameRendering += RenderPipeline_beginFrameRendering;
-		//LightweightRenderPipeline.OnRenderingSingleCamera += LightweightRenderPipeline_OnRenderingSingleCamera;
 	}
 
 	private void LightweightRenderPipeline_OnRenderingSingleCamera(ScriptableRenderContext context, Camera camera, CommandBuffer cmd)
 	{
-		//print("asdagg");
-		//context.
-		print(cmd.name);
-		//cmd.Blit()
-		//cmd.Blit
 	}
 
 	[ContextMenu("Disable")]
@@ -43,27 +42,16 @@ public class SRPEventsTest : MonoBehaviour
 		{
 			UnityEngine.Experimental.Rendering.RenderPipeline.beginCameraRendering -= RenderPipeline_beginCameraRendering;
 			UnityEngine.Experimental.Rendering.RenderPipeline.beginFrameRendering -= RenderPipeline_beginFrameRendering;
-			//LightweightRenderPipeline.OnRenderingSingleCamera -= LightweightRenderPipeline_OnRenderingSingleCamera;
 		}
 		catch { }
 	}
 
 	private void RenderPipeline_beginFrameRendering(Camera[] obj)
 	{
-		//print("beginFrameRendering");
 	}
 
 	private void RenderPipeline_beginCameraRendering(Camera camera)
 	{
-		//print("beginCameraRendering");obj.
-		//var cmds = camera.GetCommandBuffers(CameraEvent);
-		//CommandBuffer newcmd = new CommandBuffer();
-		//camera.AddCommandBuffer(CameraEvent.BeforeImageEffects, newcmd);
-		//camera.AddCommandBuffer(CameraEvent.AfterEverything, newcmd);
 		print(string.Format("Camera: [{0}] has {1}", camera.name, camera.GetCommandBuffers(CameraEvent).Length));
-		//print()
-		//print(obj.activeTexture.width);
-		//newcmd.Blit(obj.activeTexture, )
-		//print("cmds count: " + cmds.Length);
 	}
 }
